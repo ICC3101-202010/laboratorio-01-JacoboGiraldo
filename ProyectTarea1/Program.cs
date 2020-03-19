dc5fde83d39928fdc769eae6802a289c0ebf12d7
Juan Jacobo Giraldo Rodriguez <juanjacobogiraldorodriguez@Juans-MacBook-Air-2.local>
2020-03-19 15:44:49 -0300

    First Commit in Blank

---
+++ ProyectTarea1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ProyectTarea1
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
